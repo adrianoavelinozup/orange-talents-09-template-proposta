92635b5efa4f1f2f4003c653107ed48a47369b63
Adriano Avelino <adriano.avelino@zup.com.br>
2021-10-20 15:59:04 -0300

    adiciona a estrutura inicial do projeto

---
+++ src/test/java/br/com/zup/adrianoavelino/proposta/PropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.adrianoavelino.proposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
