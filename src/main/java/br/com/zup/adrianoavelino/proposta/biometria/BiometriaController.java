6db561259802e2a40c5a02136c42d0f3e4ec4cc4
Adriano Avelino <adriano.avelino@zup.com.br>
2021-10-28 08:59:49 -0300

    adiciona logs no cadastro de biometria

--- src/main/java/br/com/zup/adrianoavelino/proposta/biometria/BiometriaController.java
+++ src/main/java/br/com/zup/adrianoavelino/proposta/biometria/BiometriaController.java
@@ -3,6 +3,8 @@ package br.com.zup.adrianoavelino.proposta.biometria;
 import br.com.zup.adrianoavelino.proposta.compartilhada.excecoes.EntidadeNaoEncontradaException;
 import br.com.zup.adrianoavelino.proposta.proposta.Cartao;
 import br.com.zup.adrianoavelino.proposta.proposta.CartaoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,14 +23,20 @@ public class BiometriaController {
     @Autowired
     private BiometriaRepository biometriaRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(BiometriaController.class);
+
     @PostMapping("/{cartaoId}/biometrias")
     public ResponseEntity<?> cadastrar(@PathVariable("cartaoId") Long cartaoId,
                                        @Valid @RequestBody BiometriaRequest request,
                                        UriComponentsBuilder uriComponentsBuilder) {
         Cartao cartao = cartaoRepository.findById(cartaoId)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException("Cartão não encontrado", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                    logger.warn("Cartão id={} não encontrado", cartaoId);
+                    return new EntidadeNaoEncontradaException("Cartão não encontrado", HttpStatus.NOT_FOUND);
+                });
         Biometria biometria = request.toModel(cartao);
         biometriaRepository.save(biometria);
+        logger.info("Biometria id={} do cartão id={} adicionada com sucesso!", biometria.getId(), cartaoId);
         URI uri = uriComponentsBuilder.path("/v1/cartoes/{idCartao}/biometrias/{id}")
                 .buildAndExpand(cartaoId, biometria.getId()).toUri();
         return ResponseEntity.created(uri).build();
