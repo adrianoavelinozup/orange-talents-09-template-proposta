6ba4530242e443def4dc16aa566ecb47b49ce9fc
Adriano Avelino <adriano.avelino@zup.com.br>
2021-11-02 10:09:09 -0300

    adiciona como_saber_tudo_funcionando_corretamente

--- src/main/java/br/com/zup/adrianoavelino/proposta/compartilhada/seguranca/SecurityConfiguration.java
+++ src/main/java/br/com/zup/adrianoavelino/proposta/compartilhada/seguranca/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package br.com.zup.adrianoavelino.proposta.compartilhada.seguranca;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +11,20 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${proposta.security.escopo}")
+    private String escopo;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(request->
-            request.antMatchers("/**").authenticated()
+                request
+                        .antMatchers(HttpMethod.GET, "/v1/propostas/**").hasAuthority(escopo)
+                        .antMatchers(HttpMethod.POST, "/v1/propostas").hasAuthority(escopo)
+                        .antMatchers(HttpMethod.POST, "/v1/cartoes/**/biometrias").hasAuthority(escopo)
+                        .antMatchers(HttpMethod.POST, "/v1/cartoes/**/bloqueios").hasAuthority(escopo)
+                        .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority(escopo)
+                        .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
