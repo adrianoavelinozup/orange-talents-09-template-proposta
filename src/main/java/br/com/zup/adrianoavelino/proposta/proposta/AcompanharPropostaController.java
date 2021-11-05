16e1575c02d90f321674b3b058115d4db4a0df89
Adriano Avelino <adriano.avelino@zup.com.br>
2021-11-05 08:34:50 -0300

    adiciona logger na consulta de proposta

--- src/main/java/br/com/zup/adrianoavelino/proposta/proposta/AcompanharPropostaController.java
+++ src/main/java/br/com/zup/adrianoavelino/proposta/proposta/AcompanharPropostaController.java
@@ -1,6 +1,9 @@
 package br.com.zup.adrianoavelino.proposta.proposta;
 
 import br.com.zup.adrianoavelino.proposta.compartilhada.excecoes.EntidadeNaoEncontradaException;
+import br.com.zup.adrianoavelino.proposta.compartilhada.seguranca.Ofuscador;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,14 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/propostas")
 public class AcompanharPropostaController {
+    private final Logger logger = LoggerFactory.getLogger(AcompanharPropostaController.class);
+
     @Autowired
     private PropostaRepository propostaRepository;
 
     @GetMapping("/{propostaId}")
-    public ResponseEntity<?> pesquisar(@PathVariable Long propostaId) {
+    public ResponseEntity<Object> pesquisar(@PathVariable Long propostaId) {
         Proposta proposta = propostaRepository.findById(propostaId)
-                .orElseThrow(() -> new EntidadeNaoEncontradaException("Proposta não encontrada", HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> {
+                    logger.warn("Proposta = {} não encontrada", propostaId);
+                    return new EntidadeNaoEncontradaException("Proposta não encontrada", HttpStatus.NOT_FOUND);
+                });
+        String documentoOfucado = Ofuscador.documento(proposta.getDocumento());
+        logger.info("Proposta = {} documento {} pesquisada", propostaId, documentoOfucado);
         return ResponseEntity.ok(proposta);
     }
-
 }
